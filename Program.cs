f4103602a444944af40669c2963df730c97642c1
rumeysozdemir <rumeysozd@gmail.com>
2021-01-24 14:17:57 +0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DictionaryHomework
 {
@@ -7,22 +8,39 @@ namespace DictionaryHomework
         static void Main(string[] args)
         {
             MyDictionary<int, string> YasAd= new MyDictionary<int, string>();
+            
             YasAd.Add(15, "Arda");
             YasAd.Add(20, "Beyza");
 
-            
-            
+            var ad = YasAd.Adlar;
+            var yas = YasAd.Yaslar;   //  \n = alt alta yazdırıyor \t = yan yana yazdırıyor.
 
+            for (int i = 0; i < ad.Length; i++)
+            {
+                Console.WriteLine($"Ad: {ad[i]} \t Yas:{yas[i]}");
+
+            }
+
+            //foreach (var ad in YasAd.Adlar)
+            //{
+            //    Console.WriteLine("Ad: " + ad);
+            //}
+            //foreach (var yas in YasAd.Yaslar)
+            //{
+            //    Console.WriteLine("Yaş: " + yas);
+            //}
         }
 
 
     }
     class MyDictionary <T,U>
     {
-        T[] yaslar;
-        U[] adlar;
-       
-        public MyDictionary()
+         T[] yaslar;
+         U[] adlar;
+        
+
+
+    public MyDictionary()
         {
             yaslar = new T[0];
             adlar = new U[0];
@@ -54,26 +72,30 @@ namespace DictionaryHomework
             adlar[adlar.Length - 1] = ad;
 
             
+            
 
         }
+
+
+
         public int Length
-        {    
+        {
             get { return yaslar.Length; }
         }
         public int Length1
         {
             get { return adlar.Length; }
         }
-     
+
         public T[] Yaslar
         {
-            get { return yaslar; }  
+            get { return yaslar; }
         }
         public U[] Adlar
         {
             get { return adlar; }
         }
-         
+
 
     }
 
